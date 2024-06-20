ae17c778d96ab73e9c3ee1c03d6a52f9b289f70a
qkrskdusdlqslek <158007421+qkrskdusdlqslek@users.noreply.github.com>
2024-06-20 11:17:15 +0900

    Create led02.py

---
+++ day01/led02.py
@@ -0,0 +1,38 @@
+ import RPi.GPIO as GPIO
+ import time
+
+ red_led = 21
+ blue_led = 5
+ green_led = 6
+
+ #GPIO를 BCM모드로 설정
+ GPIO.setmode(GPIO.BCM)
+ #GPIO핀 설정(입력/출력)
+ GPIO.setup(red_led, GPIO.OUT)
+ GPIO.setup(blue_led, GPIO.OUT)
+ GPIO.setup(green_led, GPIO.OUT)
+
+ try:
+   while True:
+     GPIO.output(red_led, False)
+     GPIO.output(blue_led, True)
+     GPIO.output(green_led, True)
+     time.sleep(1)
+
+     GPIO.output(red_led, True)
+     GPIO.output(blue_led, False)
+     GPIO.output(green_led, True)
+     time.sleep(1)
+
+     GPIO.output(red_led, True)
+     GPIO.output(blue_led, True)
+     GPIO.output(green_led, False)
+     time.sleep(1)
+
+
+     GPIO.output(red_led, True)
+     GPIO.output(blue_led, True)
+     GPIO.output(green_led, True)
+     time.sleep(1)
+ except KeyboardInterrupt:   #Ctrl + c
+   GPIO.cleanup()
