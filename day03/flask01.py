04819265ffec18f21a7fc1b3422606f27244d848
qkrskdusdlqslek <158007421+qkrskdusdlqslek@users.noreply.github.com>
2024-06-24 14:50:35 +0900

    Update flask01.py

--- day03/flask01.py
+++ day03/flask01.py
@@ -1,10 +1,10 @@
  from flask import Flask
 
- app = Flask(__name__)
+ app = Flask(__name__)    # name 이름을 통한 flask 객체생성
 
- @app.route("/")
+ @app.route("/")          # 라우팅을 위한 뷰함수 등록
  def hello():
    return "Hello World"
 
- if __name__ == "__main__":
-   app.run(host="0.0.0.0")
+ if __name__ == "__main__":   # 터미널에서 직접 실행시키면 실행파일이 main으로 바뀐다
+   app.run(host="0.0.0.0", port="10111", debug=True)    # 실행을 위한 명령문으로 보면 된다
