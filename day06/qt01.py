d182dfc23de7ea88d8500271794c113afd960abd
qkrskdusdlqslek <158007421+qkrskdusdlqslek@users.noreply.github.com>
2024-06-27 15:49:22 +0900

    Update qt01.py

--- day06/qt01.py
+++ day06/qt01.py
@@ -7,11 +7,11 @@
  # windowClass
  class WindowClass(QMainWindow, form_class):
    def __init__(self):            # 생성자, 첫번째인자는 self
-     super().__init__()         # 부모클래스 생성자(QWidgets)
+     super().__init__()           # 부모클래스 생성자(QWidgets)
      self.setupUi(self)
 
  if __name__ == "__main__":
-   app = QApplication(sys.argv)
-   myWindow = WindowClass()
-   myWindow.show()
-   app.exec_()
+   app = QApplication(sys.argv) # 프로그램 실행시키는 클래스
+   myWindow = WindowClass()     # WindowClass() 인스턴스 생성
+   myWindow.show()              # 화면 보여주기
+   app.exec_()                  # 프로그램 실행
